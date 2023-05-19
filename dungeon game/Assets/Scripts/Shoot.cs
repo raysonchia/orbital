cc1521ec99af5c45f4cd4c5e528539e46ae0aa9f
raysonchia <122922486+raysonchia@users.noreply.github.com>
2023-05-19 20:41:19 +0800

    Limited the fire rate

--- dungeon game/Assets/Scripts/Shoot.cs
+++ dungeon game/Assets/Scripts/Shoot.cs
@@ -7,17 +7,20 @@ using UnityEngine.InputSystem;
 public class Shoot : MonoBehaviour
 {
     public GameObject projectile;
-    public float speed = 10;
+    public float speed = 8;
+    private float fireRate = 0.17f;
+    private float nextFire;
 
     public void shootAction()
     {
+        if (Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            GameObject bullet = Instantiate(projectile, transform.position, transform.rotation);
 
-        GameObject bullet = Instantiate(projectile, transform.position, transform.rotation);
-
-        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
-        bullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
-        
-
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 direction = (mousePos - (Vector2)transform.position).normalized;
+            bullet.GetComponent<Rigidbody2D>().velocity = direction * speed;
+        }
     }
 }
