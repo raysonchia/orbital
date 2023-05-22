9c4f2da54d6b7e916f7518ec943d218aa154d9d5
raysonchia <122922486+raysonchia@users.noreply.github.com>
2023-05-22 22:30:28 +0800

    Added flocking behaviour in enemies to prevent overlapping

--- dungeon game/Assets/Scripts/SimpleEnemyMovement.cs
+++ dungeon game/Assets/Scripts/SimpleEnemyMovement.cs
@@ -7,18 +7,24 @@ public class SimpleEnemyMovement : MonoBehaviour
     Transform player;
     public float moveSpeed;
     public EnemyScriptableObjects enemyData;
+    public float spaceBetween = 0.75f;
+    private GameObject[] otherEnemies;
 
     // Start is called before the first frame update
     void Start()
     {
         player = FindObjectOfType<PlayerMovement>().transform;
         moveSpeed = enemyData.MoveSpeed;
+        otherEnemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+
+        Separate();
+        //StopAtPlayer();
 
         // flip slug
         if (player.transform.position.x > transform.position.x)
@@ -44,5 +50,30 @@ public class SimpleEnemyMovement : MonoBehaviour
     {
         Debug.Log("attacked");
     }
+
+    private void Separate()
+    {
+        foreach(GameObject e in otherEnemies)
+        {
+            if (e != gameObject)
+            {
+                float distance = Vector3.Distance(e.transform.position, transform.position);
+                if (distance <= spaceBetween)
+                {
+                    Vector3 direction = transform.position - e.transform.position;
+                    transform.Translate(direction * Time.deltaTime);
+                }
+            }
+        }
+    }
+
+    //private void StopAtPlayer()
+    //{
+    //    if (Vector3.Distance(player.position, transform.position) >= 0.75f)
+    //    {
+    //        Vector3 direction = player.position - transform.position;
+    //        transform.Translate(direction * Time.deltaTime * moveSpeed);
+    //    }
+    //}
 
 }
