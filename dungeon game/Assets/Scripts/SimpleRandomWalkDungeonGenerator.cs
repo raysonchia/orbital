d74dee62d4376064f8b91d4d25ef9298d73ca699
Nauxe <51425614+Nauxe@users.noreply.github.com>
2023-05-26 17:11:10 +0800

    Scriptable Objects for each type of dungeon room created

---
+++ dungeon game/Assets/Scripts/SimpleRandomWalkDungeonGenerator.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+
+public class SimpleRandomWalkDungeonGenerator : AbstractDungeonGenerator
+{
+
+    [SerializeField]
+    private SimpleRandomWalkSO randomWalkParameters;
+
+    protected override void RunProceduralGeneration()
+    {
+        HashSet<Vector2Int> floorPositions = RunRandomWalk();
+        tilemapVisualiser.Clear();
+        tilemapVisualiser.PaintFloorTiles(floorPositions);
+    }
+
+    protected HashSet<Vector2Int> RunRandomWalk() 
+    {
+        var currentPosition = startPosition;
+        HashSet<Vector2Int> floorPositions = new HashSet<Vector2Int>();
+        for (int i = 0; i < randomWalkParameters.iterations; i++)
+        {
+            var path = ProceduralGenerationAlgorithms.SimpleRandomWalk(currentPosition, randomWalkParameters.walkLength);
+            floorPositions.UnionWith(path);
+            if (randomWalkParameters.startRandomlyEachIteration)
+            {
+                currentPosition = floorPositions.ElementAt(Random.Range(0, floorPositions.Count));
+            }
+        }
+        return floorPositions;
+    }
+}
+
