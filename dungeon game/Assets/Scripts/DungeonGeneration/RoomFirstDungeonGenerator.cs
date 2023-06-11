28ea7089cc94469a3659a06b591a12e4fa173832
Nauxe <51425614+Nauxe@users.noreply.github.com>
2023-06-12 03:33:36 +0800

    Dark dungeon assets used for dungeon generation

---
+++ dungeon game/Assets/Scripts/DungeonGeneration/RoomFirstDungeonGenerator.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomFirstDungeonGenerator : SimpleRandomWalkDungeonGenerator
+{
+    [SerializeField]
+    private int minRoomWidth = 4, minRoomHeight = 4;
+
+    [SerializeField]
+    private int dungeonWidth = 20, dungeonHeight = 20;
+
+    [SerializeField]
+    [Range(0, 10)]
+    private int offset = 1;
+
+    [SerializeField]
+    private bool randomWalkRooms = false;
+
+    protected override void RunProceduralGeneration()
+    {
+        CreateRooms();
+    }
+
+    private void CreateRooms()
+    {
+        var roomList = ProceduralGenerationAlgorithms.BinarySpacePartioning(
+            new BoundsInt((Vector3Int) startPosition, new Vector3Int(dungeonWidth, dungeonHeight, 0)),
+            minRoomWidth, minRoomHeight);
+
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        floor = CreateSimpleRooms(roomList);
+
+        tilemapVisualiser.PaintFloorTiles(floor);
+        WallGenerator.CreateWalls(floor, tilemapVisualiser);
+    }
+
+    private HashSet<Vector2Int> CreateSimpleRooms(List<BoundsInt> roomList)
+    {
+        HashSet<Vector2Int> floor = new HashSet<Vector2Int>();
+        foreach (var room in roomList)
+        {
+            for (int col = offset; col < room.size.x - offset; col++)
+            {
+                for (int row = offset; row < room.size.x - offset; row++)
+                {
+                    Vector2Int position = (Vector2Int)room.min + new Vector2Int(col, row);
+                    floor.Add(position);
+                }
+            }
+        }
+        return floor;
+    }
+}
