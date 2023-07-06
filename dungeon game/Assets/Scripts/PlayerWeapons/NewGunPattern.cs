6d7bb40c0fbbe85d80f9b8eabc1e0d082da0fe07
raysonchia <122922486+raysonchia@users.noreply.github.com>
2023-07-06 18:12:02 +0800

    Added new weapon

---
+++ dungeon game/Assets/Scripts/PlayerWeapons/NewGunPattern.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGunPattern : PlayerAttacks, IWeapon
+{
+    // Use this for initialization
+    public void ShootAction()
+    {
+        base.Shotty(3);
+    }
+}
