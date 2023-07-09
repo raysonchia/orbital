6c8f3ced29d99f6c4ec30393f366cfbd019aee87
raysonchia <122922486+raysonchia@users.noreply.github.com>
2023-07-10 02:59:15 +0800

    Added new weapon

---
+++ dungeon game/Assets/Scripts/PlayerWeapons/ShotgunPattern.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotgunPattern : PlayerAttacks, IWeapon
+{
+    [SerializeField]
+    private int numOfProjectiles;
+
+    // Use this for initialization
+    public void ShootAction()
+    {
+        base.Shotty(numOfProjectiles);
+    }
+}
